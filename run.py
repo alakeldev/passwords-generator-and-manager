bee0771c70905dbd220113e03be7babec27256da
Abdullah Alakel <abdullah.k.alakel@gmail.com>
2023-08-05 19:33:24 +0200

    Import the necessary modules + print App title

---
+++ run.py
@@ -0,0 +1,9 @@
+import termcolor
+import pyfiglet
+import random
+
+# Application Title with ASCII art and blue color
+app_title = pyfiglet.figlet_format("Passwords Generator", width = 100)
+print(termcolor.colored(app_title, color= "blue"))
+
+
