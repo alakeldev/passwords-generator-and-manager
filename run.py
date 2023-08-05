5f98fcd6c9f7672568d9bbde67cb6f60542897f4
Abdullah Alakel <abdullah.k.alakel@gmail.com>
2023-08-05 22:59:40 +0200

    Simple Changes In get_user_name function - colored, fun doc, print msgs

--- run.py
+++ run.py
@@ -8,28 +8,28 @@ app_title = pyfiglet.figlet_format("Passwords Generator", width = 100)
 print(termcolor.colored(app_title, color= "blue"))
 
 
-print(termcolor.colored("Welcome To Your Passwords Generator Application!\n", color = "green"))
+print(termcolor.colored("Welcome To Your Passwords Generator Application!\n", color = "blue"))
 
 def get_user_name():
     """ 
     Simple Function To Get the User Full Name, 
-    Then Check On The User Name if it Follows the General Name Syntax "letters" only using (Regular Expression)    
-    Say Hi To User though Simple nice Message
+    Then Check On The User Name if it Follows the General Name Syntax "letters" only => using (Regular Expression)    
+    Say Hi To User through Simple nice Message
     """
     while True:
         fname = input("Please Enter Your First Name: ").strip().capitalize()
         lname = input("Please Enter Your Last Name: ").strip().capitalize()
 
         if re.search("^[A-z]+$", fname and lname):
-            print(f"Hi {fname} {lname}, Nice To See You!")
+            print(termcolor.colored(f"\nHi {fname} {lname}, Nice To See You!. \n", color = "blue"))
             break
 
         else:
             try:
                 raise ValueError(
-                    f"Please Write A Correct Name!"
+                    f"Please Write a Correct and Real Name Using Letters Only!"
                 )
             except ValueError as e:
-                print(f"Invalid Name: {e}, please try again. \n")
+                print(termcolor.colored(f"\nInvalid Name: {e}. Please try again.\n", color = "blue"))
 
 get_user_name()
