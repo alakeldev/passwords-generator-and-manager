4148c13525d2ca6ae480bfb7bc5c3f21801d7274
Abdullah Alakel <abdullah.k.alakel@gmail.com>
2023-08-11 15:11:52 +0200

    Reset my-passwords.txt + Reorgnise my code and add more three functions and parceling my code inside these functions to don't fall in spaghetti code + changes in some valueError msgs

--- run.py
+++ run.py
@@ -7,19 +7,17 @@ import codecs
 
 def get_user_name():
     """
-    Simple Function To Get the User Full Name,
-    Then Check On The User Name if it Follows the General Name Syntax "letters"
-    through (Regular Expression), also more than two letters.
-    Say Hi to user_name through Simple nice Message
+    While: to get the user first & last name
+    If: to check on fname & lname that are following general name syntax
+    (letters & more than one letter)
+    try: to raise a valueError if user enter other than letters
     """
     while True:
         fname = input("Please Enter Your First Name: ").strip().capitalize()
         lname = input("Please Enter Your Last Name: ").strip().capitalize()
-
         if re.search("^[A-z]+$", fname) and re.search("^[A-z]+$", lname) and len(fname) > 1 and len(lname) > 1:
             print(termcolor.colored(f"\nHi {fname} {lname}, Nice To See You!. \n", color="blue"))
             break
-
         else:
             try:
                 raise ValueError(
@@ -31,52 +29,18 @@ def get_user_name():
 
 def pwd_generator():
     """
-    Function to ask the user if he wants to generate a new password? with three scenarios as per below:
-    - if "yes": it prompts the user to enter the desired length of a new password
-    Then It's going to check if the user value is a number between 4 and 70 to accept it
-    and Generate the new password from the static variable that we provided.
-    Finally it will ask the user if wants another new password to generate a new one again.
-    - If "No": It will skip this part of the app and jump to the part of PWD Manager.
-    - If "Other Values" it will show user msg(invalid value) to guide the user.
-    Then starts again and push the user to put the expected values to proceed.
+    While: to ask the user if wants to generate new password
+    If: to check the three possible scenarios:(yes,no,other)
+    Try: to raise a valueError if user enter other values or empty
     """
     while True:
         ask_user = input("Do You Want to Generate a New Password?\n'y/n' or 'yes/no': ").lower().strip()
         if ask_user == "y" or ask_user == "yes":
-
-            while True:
-                pwd_len = input("How many Chars do you want in your password?\n'Min = 4' & 'Max = 40': ").strip()
-                print("\n", end="")
-                if re.search("^\d+$", pwd_len) and 3 < int(pwd_len) < 41:
-                    PWD_CHARACTERS = "qwertzuiopü+asdfghjklöä#yxcvbnm,.-!§$%&/()=?ß#><-*/@\12345678}9[]ß{+ZQ"
-                    pwd = ""
-                    new_password = termcolor.colored(pwd.join(random.sample(PWD_CHARACTERS, int(pwd_len))), color="blue")
-                    print(f"Your New Password Is: {new_password} \n")
-
-                    while True:
-                        another_pwd = input("Do you want Another new Password?\n'y/n' or 'yes/no': ").lower().strip()
-                        if another_pwd == "yes" or another_pwd == "y":
-                            break
-                        elif another_pwd == "no" or another_pwd == "n":
-                            return False
-                        else:
-                            try:
-                                raise ValueError(
-                                    f"Please enter yes/no or y/n. Sorry! no other values available"
-                                )
-                            except ValueError as e:
-                                print(termcolor.colored(f"\nInvalid Value: {e}.\n", color="blue"))
-                else:
-                    try:
-                        raise ValueError(
-                            f"Sorry, the maximum Characters that can be generated are between 4 to 40 Chars only!\nNo other Values accepted"
-                            )
-                    except ValueError as e:
-                        print(termcolor.colored(f"Invalid Value: {e}. Please try again.\n", color="blue"))
-
+            pwd_chars_number()
+            break
         elif ask_user == "n" or ask_user == "no":
+            pwd_manager_start()
             return False
-
         else:
             try:
                 raise ValueError(
@@ -86,12 +50,61 @@ def pwd_generator():
                 print(termcolor.colored(f"\nInvalid Value: {e}.\n", color="blue"))
 
 
-def pwd_manager():
+def pwd_chars_number():
     """
-    a Function runs:
-    while condition that asks the user if wants to start the PWD Manager App
-    also a nested while condition to check with the user if wants to save a new password or even
-    to check-view the previous ones.
+    While: to ask the user how many Chars wants in new PWD
+    If: to control user input (only numbers and between 3-40)
+    Try: to raise a valueError if user enter other than
+    (only numbers and between 4-40)
+    """
+    while True:
+        pwd_len = input("How many Chars do you want in your password?\n'Min = 4' & 'Max = 40': ").strip()
+        print("\n", end="")
+        if re.search("^\d+$", pwd_len) and 3 < int(pwd_len) < 41:
+            PWD_CHARACTERS = "qwertzuiopü+asdfghjklöä#yxcvbnm,.-!§$%&/()=?ß#><-*/@\12345678}9[]ß{+ZQ"
+            pwd = ""
+            new_password = termcolor.colored(pwd.join(random.sample(PWD_CHARACTERS, int(pwd_len))), color="blue")
+            print(f"Your New Password Is: {new_password} \n")
+            pwd_second_generator()
+            return False
+        else:
+            try:
+                raise ValueError(
+                    f"Please Enter Only Numbers and Only Between(4 to 40).\n"
+                    )
+            except ValueError as e:
+                print(termcolor.colored(f"Invalid Value: {e}Please try again.\n", color="blue"))
+
+
+def pwd_second_generator():
+    """
+    While: to check with the user if wants another new PWD
+    after the first one that generated.
+    If: to check the three possible scenarios:(yes,no,other)
+    Try: to raise a valueError if user enter other values or empty
+    """
+    while True:
+        another_pwd = input("Do you want Another new Password?\n'y/n' or 'yes/no': ").lower().strip()
+        if another_pwd == "yes" or another_pwd == "y":
+            pwd_chars_number()
+            return False
+        elif another_pwd == "no" or another_pwd == "n":
+            pwd_manager_start()
+            return False
+        else:
+            try:
+                raise ValueError(
+                    f"Please enter yes/no or y/n. Sorry! no other values available"
+                    )
+            except ValueError as e:
+                print(termcolor.colored(f"\nInvalid Value: {e}.\n", color="blue"))
+
+
+def pwd_manager_start():
+    """
+    While: to ask the user if wants to run the second app
+    If: to check the three possible scenarios:(yes,no,other)
+    Try: to raise a valueError if user enter other values or empty
     """
     while True:
         passwords_manager = input("\nDo you want to start the Passwords Manager Application?\n'y/n' or 'yes/no': ").lower().strip()
@@ -99,37 +112,10 @@ def pwd_manager():
             app_second_title = pyfiglet.figlet_format("Passwords Manager", font="small", width=100)
             print(termcolor.colored(app_second_title, color="blue"))
             print(termcolor.colored("Welcome To Your Passwords Manager Application!\n", color="blue"))
-
-            while True:
-                user = input("Do you want to save a new password or only view the previous ones?\n'save' or 'view': ").lower().strip()
-                if user == "save":
-                    user_name = input("Username: ").strip()
-                    new_password = input("Password: ").strip()
-                    if user_name != "" and new_password != "" and len(user_name) > 2 and len(new_password) > 2:
-                        password_file = codecs.open("my-passwords.txt", "a", encoding="utf-8")
-                        password_file.write(f"Username: {user_name} | Password: {new_password}\n")
-                        password_file.close()
-                        print(termcolor.colored("\nAdded Successfully!.\n", color="blue"))
-                        continue
-                    else:
-                        print(termcolor.colored("\nThe empty field or even one-two Chars are NOT accepted as a Values.\nAgain please check your choice!\n", color="blue"))
-                        continue
-                elif user == "view":
-                    print("\nThe following are the saved Usernames and Passwords related to your accounts: ")
-                    password_file = codecs.open("my-passwords.txt", "r", encoding="utf-8")
-                    for pwd in password_file.readlines():
-                        print(termcolor.colored(pwd, color="blue"), end="")
-                    password_file.close()
-                    return False
-                else:
-                    try:
-                        raise ValueError(
-                            f"Please enter 'save' or 'view'. Sorry! no other choices"
-                            )
-                    except ValueError as e:
-                        print(termcolor.colored(f"\nInvalid Value: {e}. Please try again.\n", color="blue"))
-
+            pwd_manager_run()
+            break
         elif passwords_manager == "n" or passwords_manager == "no":
+            print(termcolor.colored("\nOk, See You Later!.", color="blue"))
             return False
         else:
             try:
@@ -140,15 +126,53 @@ def pwd_manager():
                 print(termcolor.colored(f"\nInvalid Value: {e}.", color="blue"))
 
 
+def pwd_manager_run():
+    """
+    While: to ask user if wants to save new pwd or only view the old ones
+    If: to check the three possible scenarios:(save,view,other)
+    If nested inside save scenario: to check the user inputs
+    (not empty & more than two Chars)
+    For inside view scenario: to loop on each line inside the file
+    and print it on terminal
+    Try: to raise a valueError if user enter other than (Save and View)
+    """
+    while True:
+        user = input("Do you want to save a new password or only view the previous ones?\n'save' or 'view': ").lower().strip()
+        if user == "save":
+            user_name = input("Username: ").strip()
+            new_password = input("Password: ").strip()
+            if user_name != "" and new_password != "" and len(user_name) > 2 and len(new_password) > 2:
+                password_file = codecs.open("my-passwords.txt", "a", encoding="utf-8")
+                password_file.write(f"Username: {user_name} | Password: {new_password}\n")
+                password_file.close()
+                print(termcolor.colored("\nAdded Successfully!.\n", color="blue"))
+            else:
+                print(termcolor.colored("\nThe empty field or even one-two Chars are NOT accepted as a Values.\nAgain please check your choice!\n", color="blue"))
+                continue
+        elif user == "view":
+            print("\nThe following are the saved Usernames and Passwords related to your accounts: ")
+            password_file = codecs.open("my-passwords.txt", "r", encoding="utf-8")
+            for pwd in password_file.readlines():
+                print(termcolor.colored(pwd, color="blue"))
+                password_file.close()
+            break
+        else:
+            try:
+                raise ValueError(
+                    f"Please enter 'save' or 'view'. Sorry! no other choices"
+                    )
+            except ValueError as e:
+                print(termcolor.colored(f"\nInvalid Value: {e}. Please try again.\n", color="blue"))
+
+
 def main():
-    ''' Function to Run All App Functions inside it '''
+    ''' Main Function to run the main App code inside it '''
+    app_title = pyfiglet.figlet_format("Passwords Generator", font="small", width=100)
+    print(termcolor.colored(app_title, color="blue"))
+    print(termcolor.colored("Welcome To Your Passwords Generator Application!\n", color="blue"))
     get_user_name()
     pwd_generator()
-    pwd_manager()
+    print(termcolor.colored("Goodbye! Thank You For Using Our Applications.\n", color="blue"))
 
 
-app_title = pyfiglet.figlet_format("Passwords Generator", font="small", width=100)
-print(termcolor.colored(app_title, color="blue"))
-print(termcolor.colored("Welcome To Your Passwords Generator Application!\n", color="blue"))
 main()
-print(termcolor.colored("\nGoodbye! Thank You For Using Our Applications.\n", color="blue"))
